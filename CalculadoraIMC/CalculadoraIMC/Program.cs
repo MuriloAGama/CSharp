8e822c786bac236641db8347502742cedf69700c
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-04-05 21:47:57 -0300

    @MuriloAGama [Task: Intro CSharp] {05/04/2022 21:47}

---
+++ CalculadoraIMC/CalculadoraIMC/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int age;
+            double Weight;
+            double Height;
+            double Imc;
+
+
+            Console.WriteLine("Digite seu nome: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Olá " + Name + " Digite seu peso: ");
+            Weight = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Olá " + Name + " Digite sua altura: ");
+            Height = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Imc = Weight / (Height * Height);
+
+            if (Imc < 18.5)
+            {
+
+                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Abaixo do peso ");
+
+            }
+            else if (Imc < 25)
+            {
+
+                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Peso Normal ");
+            }
+            else if (Imc < 30)
+            {
+
+                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Sobrepeso ");
+
+            }
+            else
+            {
+                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Obesidade ");
+            }
+        }
+    }
+}
