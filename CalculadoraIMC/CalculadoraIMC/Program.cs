e23d20d69593d5575d0a7aaa424352aeca3be94a
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-04-07 15:58:34 -0300

    @MuriloAGama [Task:Conteudo C#] {07/04/2022 15:58}

--- CalculadoraIMC/CalculadoraIMC/Program.cs
+++ CalculadoraIMC/CalculadoraIMC/Program.cs
@@ -18,12 +18,12 @@ namespace CalculadoraIMC
             Name = Console.ReadLine();
 
             Console.WriteLine("Digite sua idade: ");
-            Name = Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Olá " + Name + " Digite seu peso: ");
+            Console.WriteLine($"Olá {Name} Digite seu peso: ");
             Weight = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Olá " + Name + " Digite sua altura: ");
+            Console.WriteLine($"Olá {Name} Digite sua altura: ");
             Height = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Imc = Weight / (Height * Height);
@@ -31,23 +31,23 @@ namespace CalculadoraIMC
             if (Imc < 18.5)
             {
 
-                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Abaixo do peso ");
+                Console.WriteLine($"Olá {Name} Sua massa muscular encontra-se na faixa: Abaixo do peso ");
 
             }
             else if (Imc < 25)
             {
 
-                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Peso Normal ");
+                Console.WriteLine($"Olá {Name} Sua massa muscular encontra-se na faixa: Peso Normal ");
             }
             else if (Imc < 30)
             {
 
-                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Sobrepeso ");
+                Console.WriteLine($"Olá {Name} Sua massa muscular encontra-se na faixa: Sobrepeso ");
 
             }
             else
             {
-                Console.WriteLine("Olá " + Name + " Sua massa muscular encontra-se na faixa: Obesidade ");
+                Console.WriteLine($"Olá {Name} Sua massa muscular encontra-se na faixa: Obesidade ");
             }
         }
     }
---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite qual tabuada deseja visualizar : ");
+            int InputNum = Convert.ToInt32(Console.ReadLine());
+            tab(InputNum);
+        }
+        public static void tab(int num)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i} ");
+            }
+        }
+    }
+}
